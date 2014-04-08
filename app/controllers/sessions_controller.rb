cdf4d0724707dba99b30715400d7210187daa097
William Tandiawan <william.tandiawan@gmail.com>
2014-04-08 16:31:06 -0500

    finish user edit && exercises

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-   user = User.find_by(email: params[:email].downcase)
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      sign_in user
-      redirect_to user
+	sign_in user
+	redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination' 
       render 'new'
     end
   end
 
- def destroy
+  def destroy
     sign_out
     redirect_to root_url
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,80 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
   end
 
   def new
-    @user = User.new
+    if signed_in?
+	redirect_to root_path
+    else
+      @user = User.new
+    end
   end
-   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+
+  def create
+    if signed_in?
+      redirect_to root_path
+    else
+      @user = User.new(user_params)    # Not the final implementation!
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
 
+  def destroy
+    if current_user.admin? && User.find(params[:id])==current_user
+      flash[:error] = "Cannot delete your own admin account."
+    else
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    end
+  end
 
   private
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def signed_in_user
+     store_location
+     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
