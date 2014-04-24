c6563c372664f4f9c843a655ed291e35d91d78ee
William Tandiawan <william.tandiawan@gmail.com>
2014-04-24 13:42:04 -0500

    11.2.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,15 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+
   it { should be_valid}
   it { should_not be_admin }
 
@@ -35,6 +44,8 @@ describe User do
         before { @user.name = "a" * 51}
         it { should_not be_valid }
   end
+
+ 
 
   describe "when email format is invalid" do
     it "should be invalid" do
@@ -146,4 +157,28 @@ end
 
   end # end microposts
 
+   describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+    
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+    
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+  end
 end
